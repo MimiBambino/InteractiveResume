b32aba7cd104ef0fe05b04e7dfe2b66ba7e47e21
Cynthia <cynthiaodonnell@cynthia.homenet.telecomitalia.it>
2014-10-09 12:53:37 +0200

    fix header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 var name = "Cynthia O'Donnell";
 var role = "Front End Web Developer";
 var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderName.replace("%data%", role);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
